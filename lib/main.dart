efffa291cea70ed65440f95909da4d428ba30513
reika178 <rechiko178@gmail.com>
2020-10-31 08:08:40 +0900

    ログイン機能
    10/31

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,23 +10,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Todo App',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TodoListPage(),
+      home: MyAuthPage(),
     );
   }
 }
 
-class TodoListPage extends StatelessWidget {
+class MyAuthPage extends StatefulWidget {
+  @override
+  _MyAuthPageState createState() => _MyAuthPageState();
+}
+
+class _MyAuthPageState extends State<MyAuthPage> {
+
+  String newUserEmail = "";
+  String newUserPassword = "";
+  String infoText = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('リスト一覧画面')
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: "メールアドレス"),
+              onChanged: (String value) {
+                setState(() {
+                  newUserEmail = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "パスワード"),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  newUserPassword = value;
+                });
+              },
+            ),
+          RaisedButton(
+            onPressed: () async {
+              try {
+                final FirebaseAuth auth = FirebaseAuth.instance;
+                final AuthResult result =
+                    await auth.createUserWithEmailAndPassword(
+                      email: newUserEmail,
+                      password: newUserPassword,
+                    );
+                final FirebaseUser user = result.user;
+                setState(() {
+                  infoText = "登録OK:${user.email}";
+                });
+              } catch (e) {
+                setState(() {
+                  infoText = "登録NG:${e.message}";
+                });
+              }
+            },
+            child: Text("ユーザー登録"),
+          ),
+          Text(infoText)
+          ],
+        ),
       ),
     );
   }
 }
+
