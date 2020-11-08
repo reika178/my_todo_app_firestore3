22546dd332b38328ba38d54a45ad231721ae52bb
reika178 <rechiko178@gmail.com>
2020-11-08 10:26:04 +0900

    とりあえず完成　スワイプして消したい
    11/8

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  // 最初に表示するWidget
   runApp(MyApp());
 }
 
+// 更新可能なデータ
 class UserState extends ChangeNotifier {
   FirebaseUser user;
 
@@ -17,38 +19,48 @@ class UserState extends ChangeNotifier {
 }
 
 class MyApp extends StatelessWidget {
+  // ユーザーの情報を管理するデータ
   final UserState userState = UserState();
 
   @override
   Widget build(BuildContext context) {
+    // ユーザー情報を渡す
     return ChangeNotifierProvider<UserState>.value(
       value: userState,
       child: MaterialApp(
+        // ラベルを消す
         debugShowCheckedModeBanner: false,
-        title: 'ChatApp',
+        // アプリ名
+        title: 'MyTodoApp',
         theme: ThemeData(
+          // テーマカラー
           primaryColor: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        // ログイン画面を表示
         home: LoginPage(),
       ),
     );
   }
 }
 
+// ログイン画面用Widget
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
+  // メッセージ表示用
   String infoText = "";
+
+  // 入力したメールアドレス・パスワード
   String email = "";
   String password = "";
 
   @override
   Widget build(BuildContext context) {
+    // ユーザー情報を受け取る
     final UserState userState = Provider.of<UserState>(context);
 
     return Scaffold(
@@ -58,16 +70,18 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              // メールアドレス入力
               TextFormField(
-                decoration: InputDecoration(labelText: 'メールアドレス'),
+                decoration: InputDecoration(labelText: 'mail address'),
                 onChanged: (String value) {
                   setState(() {
                     email = value;
                   });
                 },
               ),
+              // パスワード入力
               TextFormField(
-                decoration: InputDecoration(labelText: 'パスワード'),
+                decoration: InputDecoration(labelText: 'password'),
                 obscureText: true,
                 onChanged: (String value) {
                   setState(() {
@@ -77,29 +91,39 @@ class _LoginPageState extends State<LoginPage> {
               ),
               Container(
                 padding: EdgeInsets.all(8),
+                // メッセージ表示
                 child: Text(infoText),
               ),
               Container(
+                // ユーザー登録ボタン
                 width: double.infinity,
                 child: RaisedButton(
-                  color: Colors.blue,
+                  color: Colors.purple,
                   textColor: Colors.white,
-                  child: Text('ユーザー登録'),
+                  child: Text('sign up'),
                   onPressed: () async {
                     try {
+                      // メール/パスワードでユーザー登録
                       final FirebaseAuth auth = FirebaseAuth.instance;
-                      final AuthResult result = await auth.createUserWithEmailAndPassword(
+                      final AuthResult result =
+                          await auth.createUserWithEmailAndPassword(
                         email: email,
                         password: password,
                       );
                       final FirebaseUser user = result.user;
+
+                      // ユーザー情報を更新
                       userState.setUser(user);
+
+                      // ユーザー登録に成功した場合
+                      // todo画面に遷移+ログイン画面を破棄
                       await Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) {
-                          return ChatPage();
+                          return TodoPage();
                         }),
                       );
                     } catch (e) {
+                      // ユーザー登録に失敗した場合
                       setState(() {
                         infoText = "登録に失敗しました : ${e.message}";
                       });
@@ -109,24 +133,33 @@ class _LoginPageState extends State<LoginPage> {
               ),
               Container(
                 width: double.infinity,
+                // ログイン登録ボタン
                 child: OutlineButton(
                   textColor: Colors.blue,
-                  child: Text('ログイン'),
+                  child: Text('login'),
                   onPressed: () async {
                     try {
+                      // メール/パスワードでログイン
                       final FirebaseAuth auth = FirebaseAuth.instance;
-                      final AuthResult result = await auth.signInWithEmailAndPassword(
+                      final AuthResult result =
+                          await auth.signInWithEmailAndPassword(
                         email: email,
                         password: password,
                       );
                       final FirebaseUser user = result.user;
+
+                      // ユーザー情報を更新
                       userState.setUser(user);
+
+                      // ログインに成功した場合
+                      // todo画面に遷移+ログイン画面を破棄
                       await Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) {
-                          return ChatPage();
+                          return TodoPage();
                         }),
                       );
                     } catch (e) {
+                      // ログインに失敗した場合
                       setState(() {
                         infoText = "ログインに失敗しました : ${e.message}";
                       });
@@ -142,20 +175,26 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class ChatPage extends StatelessWidget {
+// チャット画面用Widget
+class TodoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // ユーザー情報を受け取る
     final UserState userState = Provider.of<UserState>(context);
     final FirebaseUser user = userState.user;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('チャット'),
+        title: Text('My Todo'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.close),
             onPressed: () async {
+              // ログアウト状態
+              // 内部で保持しているセッションが初期化される
+
               await FirebaseAuth.instance.signOut();
+              // ログイン画面に遷移+todo画面を破棄
               await Navigator.of(context).pushReplacement(
                 MaterialPageRoute(builder: (context) {
                   return LoginPage();
@@ -169,31 +208,41 @@ class ChatPage extends StatelessWidget {
         children: <Widget>[
           Container(
             padding: EdgeInsets.all(8),
-            child: Text('ログイン情報 : ${user.email}'),
+            child: Text('user : ${user.email}'),
           ),
           Expanded(
+            // StreamBuilder
+            // 非同期処理の結果を元にWidgetを作れる
             child: StreamBuilder<QuerySnapshot>(
+              // todo一覧を取得（非同期処理）
+              // 投稿日時でソート
               stream: Firestore.instance
-                  .collection('posts')
+                  .collection('post')
                   .orderBy('date')
                   .snapshots(),
               builder: (context, snapshot) {
+                // データが取得できた場合
                 if (snapshot.hasData) {
-                  final List<DocumentSnapshot> documents = snapshot.data.documents;
+                  final List<DocumentSnapshot> documents =
+                      snapshot.data.documents;
+                      // 取得したtodo一覧を元にリスト表示
                   return ListView(
                     children: documents.map((document) {
                       IconButton deleteIcon;
+                      // 削除ボタンを表示
                       if (document['email'] == user.email) {
                         deleteIcon = IconButton(
                           icon: Icon(Icons.delete),
                           onPressed: () async {
+                            // todoのドキュメントを削除
                             await Firestore.instance
-                                .collection('posts')
+                                .collection('post')
                                 .document(document.documentID)
                                 .delete();
                           },
                         );
                       }
+
                       return Card(
                         child: ListTile(
                           title: Text(document['text']),
@@ -204,8 +253,9 @@ class ChatPage extends StatelessWidget {
                     }).toList(),
                   );
                 }
+                // データが読み込み中の場合
                 return Center(
-                  child: Text('読み込み中...'),
+                  child: Text('loading...'),
                 );
               },
             ),
@@ -215,6 +265,7 @@ class ChatPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
+          // 追加画面に遷移
           await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               return AddPostPage();
@@ -226,22 +277,25 @@ class ChatPage extends StatelessWidget {
   }
 }
 
+// 追加用Widget
 class AddPostPage extends StatefulWidget {
   @override
   _AddPostPageState createState() => _AddPostPageState();
 }
 
 class _AddPostPageState extends State<AddPostPage> {
+  // 入力した内容
   String messageText = "";
 
   @override
   Widget build(BuildContext context) {
+    // ユーザー情報を受け取る
     final UserState userState = Provider.of<UserState>(context);
     final FirebaseUser user = userState.user;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('チャット投稿'),
+        title: Text('Add'),
       ),
       body: Center(
         child: Container(
@@ -249,9 +303,12 @@ class _AddPostPageState extends State<AddPostPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              // todo入力
               TextFormField(
-                decoration: InputDecoration(labelText: '投稿メッセージ'),
+                decoration: InputDecoration(labelText: 'Todo'),
+                // 複数行のテキスト入力
                 keyboardType: TextInputType.multiline,
+                // 最大3行
                 maxLines: 3,
                 onChanged: (String value) {
                   setState(() {
@@ -264,18 +321,21 @@ class _AddPostPageState extends State<AddPostPage> {
                 child: RaisedButton(
                   color: Colors.blue,
                   textColor: Colors.white,
-                  child: Text('投稿'),
+                  child: Text('Add'),
                   onPressed: () async {
-                    final date = DateTime.now().toLocal().toIso8601String();
-                    final email = user.email;
+                    final date =
+                        DateTime.now().toLocal().toIso8601String();
+                    final email = user.email;//AddPostPageのデータを参照
+                    // Add用ドキュメント作成
                     await Firestore.instance
-                        .collection('post')
-                        .document()
+                        .collection('post')//コレクションID指定
+                        .document()//ドキュメントID自動生成
                         .setData({
                           'text': messageText,
                           'email': email,
                           'date': date
                         });
+                    // １つ前の画面に戻る
                     Navigator.of(context).pop();
                   }
                 )
